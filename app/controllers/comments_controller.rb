82de0b02b725715f72a3a0db56620e7823fcd759
rajasekhar <rajasekhar.reddy@sourcebits.com>
2013-02-21 15:33:13 +0530

    comments checked

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -42,21 +42,7 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
   end
 
-  # POST /comments
-  # POST /comments.json
-  # def create
-  #   @comment = Comment.new(params[:comment])
-
-  #   respond_to do |format|
-  #     if @comment.save
-  #       format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-  #       format.json { render json: @comment, status: :created, location: @comment }
-  #     else
-  #       format.html { render action: "new" }
-  #       format.json { render json: @comment.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+ 
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.build(params[:comment])
@@ -75,19 +61,7 @@ class CommentsController < ApplicationController
 
   # PUT /comments/1
   # PUT /comments/1.json
-  # def update
-  #   @comment = Comment.find(params[:id])
-
-  #   respond_to do |format|
-  #     if @comment.update_attributes(params[:comment])
-  #       format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-  #       format.json { head :no_content }
-  #     else
-  #       format.html { render action: "edit" }
-  #       format.json { render json: @comment.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+ 
  def update
     @comment = Comment.find(params[:id])
     @article = @comment.article
@@ -104,16 +78,7 @@ class CommentsController < ApplicationController
 
   # DELETE /comments/1
   # DELETE /comments/1.json
-#   def destroy
-#     @comment = Comment.find(params[:id])
-#     @comment.destroy
 
-#     respond_to do |format|
-#       format.html { redirect_to comments_url }
-#       format.json { head :no_content }
-#     end
-#   end
-# end
 def destroy
     @comment = Comment.find(params[:id])
     @article = Article.find(params[:article_id])
